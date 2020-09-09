a6ad340752ea7f6c9a4f2e4b14dbcf1c2d632817
GlitchTip <111671620+GlitchTip@users.noreply.github.com>
2020-09-09 16:24:54 +0000

    Release 3.5.0

--- example/jest.config.js
+++ example/jest.config.js
@@ -1,1 +1,9 @@
-module.exports
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js'],
+  transformIgnorePatterns: ['/node_modules/'],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*.js',
+    '<rootDir>/**/*.{spec,test}.js',
+  ],
+};
