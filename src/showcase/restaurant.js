4f2be1c935a534a80e133b1cd4f24464d416d07f
GlitchTip <111671620+GlitchTip@users.noreply.github.com>
2020-10-24 08:23:02 +0000

    update uoj1109_array

--- src/showcase/restaurant.js
+++ src/showcase/restaurant.js
@@ -50,3 +50,126 @@ function restaurant({ name, image, place, time, rating, phone, website }) {
         },
         {
           type: 'box',
+          layout: 'vertical',
+          margin: 'lg',
+          spacing: 'sm',
+          contents: [
+            {
+              type: 'box',
+              layout: 'baseline',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'text',
+                  text: 'Place',
+                  color: '#aaaaaa',
+                  size: 'sm',
+                  flex: 1,
+                },
+                {
+                  type: 'text',
+                  text: place,
+                  wrap: true,
+                  color: '#666666',
+                  size: 'sm',
+                  flex: 5,
+                },
+              ],
+            },
+            {
+              type: 'box',
+              layout: 'baseline',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'text',
+                  text: 'Time',
+                  color: '#aaaaaa',
+                  size: 'sm',
+                  flex: 1,
+                },
+                {
+                  type: 'text',
+                  text: time,
+                  wrap: true,
+                  color: '#666666',
+                  size: 'sm',
+                  flex: 5,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    footer: {
+      type: 'box',
+      layout: 'vertical',
+      spacing: 'sm',
+      contents: [
+        ...(phone
+          ? [
+              {
+                type: 'button',
+                style: 'link',
+                height: 'sm',
+                action: {
+                  type: 'uri',
+                  label: 'CALL',
+                  uri: `tel:${phone}`,
+                },
+              },
+            ]
+          : []),
+        ...(website
+          ? [
+              {
+                type: 'button',
+                style: 'link',
+                height: 'sm',
+                action: {
+                  type: 'uri',
+                  label: 'WEBSITE',
+                  uri: website,
+                },
+              },
+            ]
+          : []),
+      ],
+    },
+  };
+}
+
+const goldStar =
+  'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png';
+const grayStar =
+  'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png';
+
+function getRatingContent(rating) {
+  const starCount = Math.floor(rating);
+  const starIconGold = {
+    type: 'icon',
+    size: 'sm',
+    url: goldStar,
+  };
+  const starIconGray = {
+    type: 'icon',
+    size: 'sm',
+    url: grayStar,
+  };
+
+  return [
+    ...new Array(starCount).fill(0).map(() => starIconGold),
+    ...new Array(5 - starCount).fill(0).map(() => starIconGray),
+    {
+      type: 'text',
+      text: rating.toFixed(1),
+      size: 'sm',
+      color: '#999999',
+      margin: 'md',
+      flex: 0,
+    },
+  ];
+}
+
+module.exports = restaurant;
---
+++ src/showcase/social.js
@@ -0,0 +1,8 @@
+/**
+ * @example
+ *
+ * ```js
+ * social({
+ *   images: [
+ *     'https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip7.jpg',
+ *     'https://scdn.line-apps.com/n/channel_devcen
