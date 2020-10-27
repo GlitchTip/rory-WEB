3e81ad3ddefeb12a5a33bf928a711807027b95ee
GlitchTip <111671620+GlitchTip@users.noreply.github.com>
2020-10-27 15:51:58 +0000

    update port

--- src/showcase/social.js
+++ src/showcase/social.js
@@ -5,4 +5,14 @@
  * social({
  *   images: [
  *     'https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip7.jpg',
- *     'https://scdn.line-apps.com/n/channel_devcen
+ *     'https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip8.jpg',
+ *     'https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip9.jpg',
+ *   ],
+ *   profile: 'https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip13.jpg',
+ *   username: 'brown_05',
+ *   text: 'I went to the Brown&Cony cafe in Tokyo and took a picture',
+ *   likeCount: 1140753,
+ * })
+ * ```
+ */
+function social({ images, profile, username, text, likeCount })
